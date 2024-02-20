b8f93113280a5978b1c4118767c04973e540d7f5
lorenzodlm <145830734+lorenzodlm@users.noreply.github.com>
2024-02-20 20:47:10 +0700

    creation

---
+++ Timecraft/app/src/main/java/com/example/timecraft/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.timecraft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.timecraft.ui.task.TaskFragment
+import com.example.timecraft.ui.home.HomeFragment
+import com.example.timecraft.ui.profile.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val view: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        view.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.item_tasks -> changeFragment(TaskFragment())
+                R.id.item_home -> changeFragment(HomeFragment())
+                R.id.item_profile -> changeFragment(ProfileFragment())
+                else -> false
+            }
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment): Boolean {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(fragment::class.java.name)
+            .commit()
+
+        return true
+    }
+}
